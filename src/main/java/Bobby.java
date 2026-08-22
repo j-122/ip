2e1e457b64127dd3acae675ebe3f7c9875142ca9
yue <e1525221@u.nus.edu>
2026-08-22 23:07:53 +0800

    Changed name and added greet and exit

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,18 @@
+public class Bobby {
+    public static void main(String[] args) {
+        String banner = " ____        _     _           \n"
+                + "| __ )  ___ | |__ | |__  _   _ \n"
+                + "|  _ \\ / _ \\| '_ \\| '_ \\| | | |\n"
+                + "| |_) | (_) | |_) | |_) | |_| |\n"
+                + "|____/ \\___/|_.__/|_.__/ \\__, |\n"
+                + "                         |___/ \n";
+        String greeting = "____________________________________________________________\n" +
+                banner +
+                "Hi! I'm Bobby.\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n";
+
+        String exit = "Bye Bye!";
+        System.out.println(greeting + exit);
+    }
+}
